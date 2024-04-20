90265cd8cc62c0203faf5177c74cfcb748e15bcf
stakxu <35752922+stakxu@users.noreply.github.com>
2024-04-20 14:04:35 +0800

    Create loader.go

---
+++ config/loader.go
@@ -0,0 +1,45 @@
+// config/loader.go
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	Servers []ServerConfig `json:"servers"`
+}
+
+type ServerConfig struct {
+	Result       []ResultItem `json:"result"`
+	Address      string       `json:"address"`
+	ClientSecret string       `json:"client_secret"`
+}
+
+type ResultItem struct {
+	Host       proto.Host `json:"host"`
+	ID         uint64      `json:"id"`
+	LastActive uint64      `json:"last_active"`
+	Name       string      `json:"name"`
+	Status     proto.State `json:"status"`
+}
+
+func LoadConfig(filename string) (Config, error) {
+	var config Config
+	file, err := os.Open(filename)
+	if err != nil {
+		return config, fmt.Errorf("failed to open config file: %v", err)
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		return config, fmt.Errorf("failed to decode config file: %v", err)
+	}
+
+	return config, nil
+}
