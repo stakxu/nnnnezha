866be158a0664862c921aafdcb167c9c1851c4a6
stakxu <35752922+stakxu@users.noreply.github.com>
2024-04-20 14:05:06 +0800

    Create speed.go

---
+++ network/speed.go
@@ -0,0 +1,29 @@
+// network/speed.go
+package network
+
+import (
+	"time"
+
+	"github.com/shirou/gopsutil/net"
+)
+
+func GetNetworkSpeed() (float64, error) {
+	netIO, err := net.IOCounters(true)
+	if err != nil {
+		return 0, err
+	}
+
+	var totalSent uint64
+	now := time.Now()
+
+	for _, io := range netIO {
+		totalSent += io.BytesSent
+	}
+
+	interval := now.Sub(lastTime).Seconds()
+	netSpeed := float64(totalSent-lastSent) / interval / 1024 / 1024
+	lastTime = now
+	lastSent = totalSent
+
+	return netSpeed, nil
+}
