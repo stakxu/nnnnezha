2aecf3ffbb27e2c158cc42acc1480966e0915efb
stakxu <35752922+stakxu@users.noreply.github.com>
2024-04-20 09:45:59 +0800

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,73 +1,46 @@
-package utils
+// utils/utils.go
 
-import (
-	"fmt"
-	"time"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/net"
-)
+package utils
 
 // ANSI escape codes for colors
 const (
-	colorRed    = "\033[31m"
-	colorGreen  = "\033[32m"
-	colorYellow = "\033[33m"
-	colorReset  = "\033[0m"
+	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
+	ColorYellow = "\033[33m"
+	ColorReset  = "\033[0m"
 )
 
-// PrintTime 打印当前时间
+// PrintTime prints the current time with color
 func PrintTime() {
-	fmt.Print("\033[u") // 移动光标至行首
-	fmt.Print("\033[K") // 清除当前行
+	fmt.Print("\033[u") // Move cursor to the beginning of the line
+	fmt.Print("\033[K") // Clear current line
 
-	fmt.Printf("Time: %s%s%s\n", colorYellow, time.Now().Format("2006-01-02 15:04:05"), colorReset)
+	fmt.Printf("Time: %s%s%s\n", ColorYellow, time.Now().Format("2006-01-02 15:04:05"), ColorReset)
 }
 
-// PrintSystemInfo 打印系统信息
+// PrintSystemInfo prints system information with color
 func PrintSystemInfo() {
-	// 获取 CPU 使用率
+	// Get CPU usage
 	cpuPercent, err := cpu.Percent(time.Second, false)
 	if err != nil {
 		fmt.Printf("Failed to get CPU usage: %v\n", err)
 	} else {
-		fmt.Printf("CPU Usage: %s%.2f%%%s\n", colorRed, cpuPercent[0], colorReset)
+		fmt.Printf("CPU Usage: %s%.2f%%%s\n", ColorRed, cpuPercent[0], ColorReset)
 	}
 
-	// 获取内存使用情况
+	// Get memory usage
 	memInfo, err := mem.VirtualMemory()
 	if err != nil {
 		fmt.Printf("Failed to get memory info: %v\n", err)
 	} else {
-		fmt.Printf("Memory Usage: %s%.2f%%%s\n", colorRed, memInfo.UsedPercent, colorReset)
+		fmt.Printf("Memory Usage: %s%.2f%%%s\n", ColorRed, memInfo.UsedPercent, ColorReset)
 	}
 
-	// 获取网络上行速度
-	netSpeed, err := GetNetworkSpeed()
+	// Get network upload speed
+	netSpeed, err := getNetworkSpeed()
 	if err != nil {
 		fmt.Printf("Failed to get network speed: %v\n", err)
 	} else {
-		fmt.Printf("Network Sent Speed: %s%.2f Mbps%s\n", colorRed, netSpeed, colorReset)
+		fmt.Printf("Network Sent Speed: %s%.2f Mbps%s\n", ColorRed, netSpeed, ColorReset)
 	}
-}
-
-// GetNetworkSpeed 获取网络上行速度
-func GetNetworkSpeed() (float64, error) {
-	netIO, err := net.IOCounters(false)
-	if err != nil {
-		return 0, err
-	}
-	var lastSent uint64
-	var lastTime time.Time
-	now := time.Now()
-
-	if lastTime.IsZero() {
-		lastTime = now
-		lastSent = netIO[0].BytesSent
-	} else {
-		interval := now.Sub(lastTime).Seconds()
-		netSpeed := float64(netIO[0].BytesSent-lastSent) / interval / 1024 / 1024 // Convert bytes to MB
-		return netSpeed, nil
-	}
-	return 0, nil
 }
